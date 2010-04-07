9b49c65ebe7cb46639c4f745161a090f9063c036
Tom ten Thij <git@tomtenthij.nl>
2010-04-07 21:50:21 +0100

    Fresh rails install from a Gemfile (http://tinyurl.com/gemrails), with custom .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '92a62e0354a499a61390d8d98d83f573f8ba6551ae9e7b41d3dc1e30ea64f3f38fd37c14635eaba84250fbaa0c5dde285fcae0277b01a3b7d87ecdafd5d7b4e4'
